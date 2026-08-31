a246fdaed46b1746cf0d5d0d10c77c2eb153bfc2
Gaurav Prasad <gauravprasad003@gmail.com>
2026-08-31 18:46:07 +0530

    Learnt about goroutines.

---
+++ Course-1/goroutines/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Main function started")
+
+	go printNumber(1)
+	go printNumber(2)
+	go printNumber(3)
+
+	seconds := 3
+	fmt.Printf("Waiting for %d seconds...\n", seconds)
+	time.Sleep(time.Second * time.Duration(seconds))
+
+	fmt.Println("Main function ended")
+}
+
+func printNumber(number int) {
+	for i := 1; i <= 5; i++ {
+		fmt.Println("Number", number, ":", i)
+		// sleep for 1 second
+		time.Sleep(time.Second)
+	}
+}
